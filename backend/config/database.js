9564ec3455ec22765476b417e9d2b65a92b3d9d4
karanDevstree2023 <karan.devstree@gmail.com>
2024-10-26 09:47:57 +0530

    ....

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log('Database connected');
-    }).catch((error)=>{
-        console.log(error);
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Database connected");
     })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 export default connectDB;
